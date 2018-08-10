67be7cddf9ed1bd7dcd80bfe9d9184c5a8b26de3
Amy Zhou <aymeighz@gmail.com>
2018-08-10 22:38:32 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,19 +1,15 @@
+
+
 def unsafe?(speed)
-  if speed.to_i < 40
-    return true
-  elsif speed.to_i > 60
-    return true
+  if speed > 60 
+    true
+  elsif speed < 40
+    true
   else
-    return false
+    false
   end
 end
 
-
-
 def not_safe?(speed)
-  speed.to_i < 40 ? true : false
-  speed.to_i > 60 ? true : false
-  speed.to_i > 40 && speed.to_i < 60 ? false : true
-end	
-
-
+  speed < 40 || speed > 60 ? true : false
+end
