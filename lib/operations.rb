929ce4c45109dbd71fc8818bc5d2d63fca46a3d4
DeCode2018 <decode2018@gmail.com>
2018-08-17 13:07:06 -0400

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,17 @@
 def unsafe?(speed)
-
+	if speed > 60
+		return true
+	end
+	if speed < 40
+		return true
+	end
+	else
+		return false
 end
-
-
 
 def not_safe?(speed)
-	
+	if speed > 60
+		return true
+	end
+	speed < 40 ? (return true) : (return false)
 end
-	
-
-
